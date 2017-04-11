23aa47e054460284aa7bbd6bcc75a89f96e65190
apthos <America99>
2017-04-10 18:47:32 -0700

    Added Text Messaging Capabilities

--- src/main/java/edu/hartnell/iris/Iris.java
+++ src/main/java/edu/hartnell/iris/Iris.java
@@ -5,8 +5,10 @@ import edu.hartnell.iris.data.DataManager;
 import edu.hartnell.iris.email.EmailManager;
 import edu.hartnell.iris.gui.Console;
 import edu.hartnell.iris.plugin.PluginManager;
+import edu.hartnell.iris.text.TextManager;
 import edu.hartnell.iris.utility.ResourceUtils;
 import org.jdom.Document;
+import org.jdom.Text;
 import org.jdom.input.SAXBuilder;
 
 import java.io.File;
@@ -19,6 +21,7 @@ public class Iris {
     private static CommandManager commandManager = null;
     private static PluginManager pluginManager = null;
     private static EmailManager emailManager = null;
+    private static TextManager textManager = null;
     private static DataManager dataManager = null;
 
     public static void main(String... args) {
@@ -39,9 +42,17 @@ public class Iris {
         } else {
             Iris.warn("Iris is running with emails disabled!");
         }
+        ManagerSettings tms = ManagerSettings.getFromCollection(credentials, "twil");
+        if (tms.isEnabled()) {
+            Iris.warn("Iris is running with texts enabled!");
+            textManager = new TextManager(tms.get("ACCOUNT_SID"),
+                    tms.get("MSG_SER_SID"), tms.get("AUTH_TOKEN"));
+        } else {
+            Iris.warn("Iris is running with texts disabled!");
+        }
         ManagerSettings mysqls = ManagerSettings.getFromCollection(credentials, "mysql");
-        if (mysqls.isEnabled()){
-            Iris.warn("Iris is running with emails enabled!");
+        if (mysqls.isEnabled()) {
+            Iris.warn("Iris is running with databases enabled!");
             dataManager = new DataManager(mysqls.get("HOST"), mysqls.get("PORT"),
                     mysqls.get("USER"), mysqls.get("PASS"));
         } else {
@@ -75,17 +86,24 @@ public class Iris {
     }
 
     public static EmailManager getEmailManager() {
-        if (emailManager == null){
+        if (emailManager == null) {
             Iris.report("A null version of Email manager has been retrieved!");
         }
         return emailManager;
     }
 
     public static DataManager getDataManager() {
-        if (dataManager == null){
+        if (dataManager == null) {
             Iris.report("A null version of Data manager has been retrieved!");
         }
         return dataManager;
+    }
+
+    public static TextManager getTextManager() {
+        if (textManager == null){
+            Iris.report("A null version of Text Manager has been retrieved!");
+        }
+        return textManager;
     }
 
     public static File getRuntimeLocation() throws URISyntaxException {
@@ -103,9 +121,9 @@ public class Iris {
     private static class ManagerSettings {
 
         public static ManagerSettings getFromCollection(
-                Collection<ManagerSettings> collection, String name){
-            for (ManagerSettings MS : collection){
-                if (MS.getName().equalsIgnoreCase(name)){
+                Collection<ManagerSettings> collection, String name) {
+            for (ManagerSettings MS : collection) {
+                if (MS.getName().equalsIgnoreCase(name)) {
                     return MS;
                 }
             }
@@ -116,28 +134,28 @@ public class Iris {
         private final boolean ENABLED;
         private HashMap<String, String> settings = new HashMap<>();
 
-        public ManagerSettings(String managerName, boolean enabled){
+        public ManagerSettings(String managerName, boolean enabled) {
             NAME = managerName;
             ENABLED = enabled;
         }
 
-        public void add(String setting, String value){
+        public void add(String setting, String value) {
             settings.put(setting, value);
         }
 
-        public String get(String setting){
+        public String get(String setting) {
             return settings.get(setting);
         }
 
-        public String getName(){
+        public String getName() {
             return NAME;
         }
 
-        public boolean isEnabled(){
+        public boolean isEnabled() {
             return ENABLED;
         }
 
-        
+
     }
 
     private static HashSet<ManagerSettings> collectCredentials() {
@@ -185,10 +203,18 @@ public class Iris {
             mysqls.add("PASS", mysql.getRootElement().getChildText("pass"));
             credentials.add(mysqls);
 
-            if (!TwilFile.exists()) {
+            if (! TwilFile.exists()) {
                 Iris.warn("Warning: Could not find Twilio login file, Generating new one!");
                 ResourceUtils.ExportResource("/Twil.xml", "Credentials");
             }
+            Document twil = builder.build(TwilFile);
+            ManagerSettings twils = new ManagerSettings("twil", Boolean.parseBoolean(
+                    twil.getRootElement().getAttributeValue("enabled")));
+            twils.add("ACCOUNT_SID", twil.getRootElement().getChildText("account_sid"));
+            twils.add("MSG_SER_SID", twil.getRootElement().getChildText(
+                    "messaging_service_sid"));
+            twils.add("AUTH_TOKEN", twil.getRootElement().getChildText("auth_token"));
+            credentials.add(twils);
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/edu/hartnell/iris/text/TextManager.java
+++ src/main/java/edu/hartnell/iris/text/TextManager.java
@@ -1,13 +1,37 @@
 package edu.hartnell.iris.text;
 
+import com.twilio.Twilio;
+import com.twilio.base.ResourceSet;
+import com.twilio.http.TwilioRestClient;
+import com.twilio.rest.api.v2010.Account;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.trunking.v1.trunk.PhoneNumber;
+import edu.hartnell.iris.Iris;
+
+import java.util.HashMap;
+
 public class TextManager {
 
-    private final static String ACCOUNT_SID = "AC4e7f21626c0ceb87324da2af6ea8732d";
-    private final static String AUTH_TOKEN = "46d013712367c17d3bd9f70ef9589dc2";
+    private final String ACCOUNT_SID;
+    private final String AUTH_TOKEN;
 
-    private TextManager() {
+    private final String MESSAGING_SERVICE_SID;
 
 
+    private final TwilioRestClient client;
 
+    public TextManager(String ACC_SID, String MES_SER_SID, String AUTH) {
+        ACCOUNT_SID = ACC_SID;
+        AUTH_TOKEN = AUTH;
+        MESSAGING_SERVICE_SID = MES_SER_SID;
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        client = Twilio.getRestClient();
     }
+
+    public void message(String cell, String messageBody){
+        Message message = Message.creator(new com.twilio.type.PhoneNumber(cell),
+                MESSAGING_SERVICE_SID, messageBody).create(client);
+    }
+
+
 }
